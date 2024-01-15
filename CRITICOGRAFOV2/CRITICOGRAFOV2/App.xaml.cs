9edd74bf0d40e8ed173b5e0311ab84645d219bc9
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2024-01-15 09:13:10 -0700

    RECUPERADOAgregar archivos de proyecto.

---
+++ CRITICOGRAFOV2/CRITICOGRAFOV2/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CRITICOGRAFOV2.Vista;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CRITICOGRAFOV2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new Critica();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CRITICOGRAFOV2/CRITICOGRAFOV2/Vista/Critica.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRITICOGRAFOV2.VistaModelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CRITICOGRAFOV2.Vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Critica : ContentPage
+    {
+        public Critica()
+        {
+            InitializeComponent();
+            BindingContext = new VMpatron(Navigation);
+        }
+    }
+}
